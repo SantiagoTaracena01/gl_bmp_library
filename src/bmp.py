8a65ab4a1ee833d56efddfb55856dc334540a54f
unknown <staracenapuga@gmail.com>
2022-10-17 10:48:57 -0600

    Lectura de archivos .bmp comentada en bmp.py.

--- src/bmp.py
+++ src/bmp.py
@@ -54,21 +54,37 @@ def write_bmp(filename, framebuffer, width, height, constants):
 
 # Función que lee un archivo .bmp.
 def read_bmp(path):
+
+  # Apertura del archivo .bmp a leer.
   with open(path, "rb") as image:
+
+    # Definición del ancho y alto de la imagen.
     image.seek(2 + 4 + 2 + 2)
     header_size = utils.unpack(image.read(4))
     image.seek(2 + 4 + 2 + 2 + 4 + 4)
     width = utils.unpack(image.read(4))
     height = utils.unpack(image.read(4))
+
+    # Salto del header para leer los pixeles de la imagen.
     image.seek(header_size)
+
+    # Lista de pixeles a retornar al finalizar el proceso.
     pixels = []
+
+    # Iteración sobre todas las "filas" del archivo.
     for y in range(height):
+
+      # Nueva fila de pixeles del archivo.
       pixels.append([])
-      for x in range(width):
-        b = ord(image.read(1))
-        g = ord(image.read(1))
-        r = ord(image.read(1))
-        pixels[y].append(
-          utils.color(r, g, b)
-        )
+
+      # Iteración sobre los pixeles de cada "fila".
+      for _ in range(width):
+
+        # Tonalidades azul, verde y roja del pixel leído.
+        b, g, r = (ord(image.read(1)), ord(image.read(1)), ord(image.read(1)))
+
+        # Almacenamiento del pixel en su respectiva posición en la lista.
+        pixels[y].append(utils.color(r, g, b))
+
+  # Retorno del ancho, alto y pixeles de la imagen.
   return width, height, pixels
