63235fe1aecf52b2818230766f4e9376bf55032a
unknown <staracenapuga@gmail.com>
2022-10-20 17:01:20 -0600

    SR5 finalizado, ver ./src/images/cat.bmp.

--- src/main.py
+++ src/main.py
@@ -21,8 +21,8 @@ if __name__ == "__main__":
 
   # Valores de traslación, escala y rotación del modelo.
   TRANSLATE = (0, -0.5, 0)
-  SCALE = (0.5, 0.5, 0.5)
-  ROTATE = (0, (math.pi / 6), 0)
+  SCALE = (3, 3, 3)
+  ROTATE = (0, (math.pi / 3), 0)
 
   # Instancia y creación de valores básicos del renderer.
   renderer = Renderer()
@@ -33,10 +33,10 @@ if __name__ == "__main__":
   start = time.time()
 
   # Proceso de renderización.
-  renderer.gl_look_at(Vector(0, 0, 1), Vector(0, 0, 0), Vector(0, 1, 0))
-  model_texture = Texture("./textures/ant_txs.bmp")
+  renderer.gl_look_at(Vector(0, 0, 10), Vector(0, 0, 0), Vector(0, 1, 0))
+  model_texture = Texture("./textures/cat_txs.bmp")
   renderer.gl_load_texture(model_texture)
-  renderer.gl_load_obj("./models/ant.obj", TRANSLATE, SCALE, ROTATE)
+  renderer.gl_load_obj("./models/cat.obj", TRANSLATE, SCALE, ROTATE)
   filename = renderer.gl_finish()
 
   # Impresión de resultados finales.
